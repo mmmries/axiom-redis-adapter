73eb801d769e06948a7b02ef9bc7b38d664fbbe0
Michael Ries <michael@riesd.com>
2013-09-25 18:17:45 -0600

    refactored user schema and rom setup into a shared context

--- spec/unit/axiom/adapter/redis/read_spec.rb
+++ spec/unit/axiom/adapter/redis/read_spec.rb
@@ -1,34 +1,8 @@
 require 'spec_helper'
-require 'support/user'
+require 'support/rom_user_context'
 
 describe Axiom::Adapter::Redis do
-  let(:rom){ ROM::Environment.setup(redis: 'redis://localhost:6379/0') }
-  let(:redis){ ::Redis.new(host: 'localhost', port: '6379') }
-  
-  before :each do
-    rom.schema do
-      base_relation :users do
-        repository :redis
-
-        attribute :id,   Integer
-        attribute :name, String
-
-        key :id
-      end
-    end
-
-    rom.mapping do
-      users do
-        map :id, :name
-        model User
-      end
-    end
-
-    redis.flushdb
-
-    redis.set "users-1", Marshal.dump({id: 1, name: 'John'})
-    redis.set "users-2", Marshal.dump({id: 2, name: 'Jane'})
-  end
+  include_context "rom_user"
 
   it "can find users" do
     rom.session do |s|
